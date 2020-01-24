d8d04c3f26c616affc2ac69ebe60c2312e805cb0
lee-wood <leemaewood@gmail.com>
2020-01-24 01:58:46 -0500

    added cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const userRouter = require('./users/userRouter');
 const server = express();
 const helmet = require('helmet');
+const cors = require('cors');
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 server.use(logger);
 server.use('/api/users', userRouter);
 
