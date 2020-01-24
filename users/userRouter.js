cda8fe9b17d3f6a82380b6ad11386f61cdc21490
lee-wood <leemaewood@gmail.com>
2020-01-23 20:26:33 -0500

    user post validation, able to add new user, and add new post for specific users

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const db = require('./userDb.js');
+const postDb = require('../posts/postDb.js')
 const router = express.Router();
 
+router.use(express.json());
 
-router.post('/', (req, res) => {
-  //do your magic!
+
+router.post('/', validateUser, (req, res) => {
+  const userData = req.body;
+  db.insert(userData)
+    .then(user => {
+      res.status(201).json({
+        success: true,
+        user
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: "There was an issue creating new user.",
+        err
+      })
+    });
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+
+  const id = req.params.id;
+  const body = req.body;
+
+  postDb.insert({ ...body, user_id: id })
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was an issue creating new post."
+      });
+    });
 });
 
 router.get('/', (req, res) => {
@@ -29,8 +58,31 @@ router.get('/:id', validateUserId, (req, res) => {
   res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  if(req.user) {
+    db.getUserPosts(req.params.id)
+      .then(posts => {
+        if(posts.length > 0) {
+          res.status(200).json({
+            success: true,
+            posts
+          });
+        } else {
+          res.status(200).json({
+            success: true,
+            message: "This user has no posts."
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: "There was an error retrieving this users posts.",
+          error: err
+        });
+      })
+  } else {
+    console.log("error")
+  }
 });
 
 router.delete('/:id', (req, res) => {
@@ -61,11 +113,33 @@ function validateUserId(req, res, next) {
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const data = req.body;
+  if(!data) {
+    res.status(400).json({
+      message: "missing user data"
+    });
+  } else if(!data.name) {
+    res.status(400).json({
+      message: "missing reuired name field"
+    });
+  }else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const data = req.body;
+  if(!data) {
+    res.status(400).json({
+      message: "missing post data"
+    });
+  } else if(!data.text) {
+    res.status(400).json({
+      message: "missing required text field"
+    });
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
